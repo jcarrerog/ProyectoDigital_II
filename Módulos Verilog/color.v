b0f1382ce6edc0e6a51e42fc9866d433da14f342
jcarrerog <42346425+jcarrerog@users.noreply.github.com>
2021-08-03 17:20:03 -0500

    Update color.v

--- Módulos Verilog/color.v
+++ Módulos Verilog/color.v
@@ -1,6 +1,7 @@
 module color(
 	 input p_clock,
 	 input [15:0] pixel_data,
+	 input colr,
 	 output [4:0] R,
 	 output [5:0] G,
 	 output [4:0] B,
@@ -9,8 +10,11 @@ module color(
 	 
 	 reg [2:0] y;
 	 
-	 always @(negedge p_clock)
+	 always @(posedge p_clock)
     begin
+	 if(colr)
+	 begin
+	 
 	 if(R>17 && G<20 && B<11)
 	 begin
 	 y[2] = 1; // Color Rojo
@@ -37,6 +41,7 @@ module color(
 	 y[0] = 1; // Color Azul
 	 y[1] = 1; // Color Verde
 	 y[2] = 1; // Color Rojo
+	 end
 	 end
 	 end
 	 
