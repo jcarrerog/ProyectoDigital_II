2d4a3ed2cebcb92560e188386d0ab9f8c4a908c8
jcarrerog <42346425+jcarrerog@users.noreply.github.com>
2021-08-03 17:19:23 -0500

    Update cam_otr.v

--- Módulos Verilog/cam_otr.v
+++ Módulos Verilog/cam_otr.v
@@ -3,6 +3,7 @@ module cam_otr(
 	 input p_clock_c,
     input rst_sw,
 	 input pwdn_sw,
+	 input cam_ot,
 	 output rst,
 	 output pwdn,
 	 output reg p_clock,
@@ -14,6 +15,9 @@ module cam_otr(
  
     always @(posedge clk)
     begin
+	 if(cam_ot)
+	 begin
+	 
     contador = contador + 1; 
     if(contador == 5)
     begin
@@ -21,10 +25,14 @@ module cam_otr(
       xclk = ~xclk; 
     end
   end
+  end
   
 	 always @ (clk)
 	 begin
+	 if(cam_ot)
+	 begin
 	 p_clock = p_clock_c;
+	 end
 	 end
   
   assign rst = (rst_sw) ? 0:1;
